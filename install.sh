09b23d4c21a41a9381e172955c5124ba59514737
gustawdaniel <gustaw.daniel@gmail.com>
2017-01-30 07:17:25 +0100

    Test 3
    Sudo added

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get install sqlite3 g++ mono-mcs openjdk-9-jdk-headless mysql-server gfortran fp-compiler r-base nodejs-legacy ruby php
+apt-get update -y
+apt-get install -y sqlite3 g++ mono-mcs openjdk-9-jdk-headless mysql-server gfortran fp-compiler r-base nodejs-legacy ruby php
 
-cpan install DBI DBD::SQLite Text::CSV_XS
+cpan install DBI DBD::SQLite Text::CSV_XS -y
 
 sqlite3 log/log.db \
 "create table IF NOT EXISTS log (
