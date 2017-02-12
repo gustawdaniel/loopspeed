569f1ccdf844a1ca0c5591205ec2967deb638553
gustawdaniel <gustaw.daniel@gmail.com>
2017-02-12 11:27:25 +0100

    Install

--- install.sh
+++ install.sh
@@ -1,39 +1,24 @@
 #!/usr/bin/env bash
 
-echo "\n\n\nBASH START\n\n\n"
+apt-get update -y
+apt-get install -y g++ mono-mcs openjdk-9-jdk-headless gfortran fp-compiler r-base nodejs-legacy ruby php
+apt-get install -y sqlite3 bc git mysql-client
+cpan install DBI DBD::SQLite Text::CSV_XS
 
-# no prompt password mysql installation
-#http://stackoverflow.com/questions/7739645/install-mysql-on-ubuntu-without-password-prompt
+sqlite3 log/log.db \
+"create table IF NOT EXISTS log (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    size UNSIGNED INTEGER,
+    time DECIMAL(12,6),
+    git CHAR(41)
+);"
 
-#apt-get update -y
-
-#echo "mysql-server mysql-server/root_password password ''" | debconf-set-selections
-#echo "mysql-server mysql-server/root_password_again password ''" | debconf-set-selections
-#apt-get install -y mysql-server
-
-#apt-get install -y g++ mono-mcs openjdk-9-jdk-headless gfortran fp-compiler r-base nodejs-legacy ruby php
-#
-#apt-get install -y sqlite3 bc git
-#
-#cpan install DBI DBD::SQLite Text::CSV_XS
-#
-#sqlite3 log/log.db \
-#"create table IF NOT EXISTS log (
-#    id INTEGER PRIMARY KEY,
-#    name VARCHAR(255),
-#    size UNSIGNED INTEGER,
-#    time DECIMAL(12,6),
-#    git CHAR(41)
-#);"
-#
-#sqlite3 log/log.db \
-#"create table result (
-#    name varchar(255),
-#    a real,
-#    b real,
-#    ea real,
-#    eb real
-#);"
-#
-#
-#echo "\n\n\nBASH END\n\n\n"
+sqlite3 log/log.db \
+"create table result (
+    name varchar(255),
+    a real,
+    b real,
+    ea real,
+    eb real
+);"
