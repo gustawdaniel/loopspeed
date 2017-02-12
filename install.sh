ee9180335fde3b067059feea203f933accf9faf9
gustawdaniel <gustaw.daniel@gmail.com>
2017-02-12 12:18:19 +0100

    Php and Java

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 apt-get update -y
-apt-get install -y g++ mono-mcs openjdk-9-jdk-headless gfortran fp-compiler r-base nodejs-legacy ruby php
+apt-get install -y g++ mono-mcs  gfortran fp-compiler r-base nodejs-legacy ruby
+apt-get install -y openjdk-9-jdk-headless
+apt-get install -y php
 apt-get install -y sqlite3 bc git mysql-client curl
 apt-get install -y libtext-csv-perl libdbi-perl libdbd-sqlite3-perl
-cpan install DBI DBD::SQLite Text::CSV_XS
+#cpan install DBI DBD::SQLite Text::CSV_XS
 
 sqlite3 log/log.db \
 "create table IF NOT EXISTS log (
