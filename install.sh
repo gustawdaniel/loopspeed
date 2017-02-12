499181e3fc2bd0e8080d357f23b25a6a055cbd63
gustawdaniel <gustaw.daniel@gmail.com>
2017-02-12 12:47:04 +0100

    Languages to gitlab ci

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get update -y
-apt-get install -y g++ mono-mcs  gfortran fp-compiler r-base nodejs-legacy ruby
-apt-get install -y openjdk-9-jdk
-apt-get install -y php-cli
-apt-get install -y python
+
 apt-get install -y sqlite3 bc git mysql-client curl
 apt-get install -y libtext-csv-perl libdbi-perl libdbd-sqlite3-perl
 #cpan install DBI DBD::SQLite Text::CSV_XS
